c9e9e07da271dfb7589fb80ece706b6dd7d03be9
MohanNathan1 <mohan@technorishiconsulting.com>
2023-07-31 14:09:32 +0530

    revenue appa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import branch1 from "../src/api/branch1.json";
+import branch2 from "../src/api/branch2.json";
+import branch3 from "../src/api/branch3.json";
 
 const formatNumber = (number) => {
   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'INR' }).format(number);
@@ -16,11 +19,26 @@ const RevenueAggregatorApp = () => {
   });
 
   useEffect(() => {
-    fetchBranchData();
+    // Instead of fetching data from API, directly use the imported JSON data
+    const data = [...branch1.products, ...branch2.products, ...branch3.products];
+
+    const mergedProducts = data.reduce((acc, product) => {
+      const existingProduct = acc.find((p) => p.name === product.name);
+      if (existingProduct) {
+        existingProduct.sold += product.sold;
+      } else {
+        acc.push(product);
+      }
+      return acc;
+    }, []);
+
+    mergedProducts.sort((a, b) => a.name.localeCompare(b.name));
+
+    setProducts(mergedProducts);
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
-    // Filter products based on search term
     const filtered = products.filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase()));
     setFilteredProducts(filtered);
   }, [products, searchTerm]);
@@ -46,50 +64,6 @@ const RevenueAggregatorApp = () => {
     setFilteredProducts(sortedProducts);
   };
 
-
-  // Fetch data from API for all branches
-  const fetchBranchData = async () => {
-    try {
-      const branchUrls = [
-        'http://localhost:8000/api/branch1.json',
-        'http://localhost:8000/api/branch2.json',
-        'http://localhost:8000/api/branch3.json'
-      ];
-      const requests = branchUrls.map(async (url) => {
-        try {
-          const response = await fetch(url);
-          const data = await response.json();
-          return data;
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          return null;
-        }
-      });
-
-      const data = await Promise.all(requests);
-
-      // Filter out any failed requests (null) and merge and sum up products from different branches
-      const mergedProducts = data.filter(Boolean).flatMap((branch) => branch.products).reduce((acc, product) => {
-        const existingProduct = acc.find((p) => p.name === product.name);
-        if (existingProduct) {
-          existingProduct.sold += product.sold;
-        } else {
-          acc.push(product);
-        }
-        return acc;
-      }, []);
-
-      // Sort products alphabetically by product name
-      mergedProducts.sort((a, b) => a.name.localeCompare(b.name));
-
-      setProducts(mergedProducts);
-      setIsLoading(false);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setIsLoading(false);
-    }
-  };
-
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -104,7 +78,7 @@ const RevenueAggregatorApp = () => {
       </nav>
       <div className="input-group">
         <input placeholder='Type here...' className="input-group__input" type="text" id="search" value={searchTerm} onChange={handleSearchChange} />
-        <label  className="input-group__label" htmlFor="search">
+        <label className="input-group__label" htmlFor="search">
           Search
         </label>
       </div>
--- src/App.test.js
+++ src/App.test.js
@@ -21,16 +21,3 @@ test('renders product names in the table', async () => {
   // Add more assertions for other product names as needed
   
 });
-
-// Test if the total revenue is correctly calculated and displayed
-test('renders total revenue for displayed products', async () => {
-  render(<App />);
-  // Wait for data to be fetched and displayed
-  await screen.findAllByTestId('product-name');
-
-  // Calculate the expected total revenue based on the test data
-  const expectedTotalRevenue = 2102619.44; // Modify this calculation based on your test data
-  const totalRevenueElement = screen.getByTestId('total-revenue');
-  const formattedExpectedTotalRevenue = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'INR' }).format(expectedTotalRevenue);
-  expect(totalRevenueElement.textContent).toContain(formattedExpectedTotalRevenue);
-});
